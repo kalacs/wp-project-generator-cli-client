b895e3d29cc262a9be2c8b5ef459871f7368568b
Zoltan Kakuk <kakuk.zoltan@e-vista.hu>
2019-10-28 13:32:13 +0100

    Add service index

---
+++ commands/project/sevices/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Text} from 'ink';
+
+/// Project index
+const ServiceIndex = () => <Text>This is index of service command</Text>;
+
+
+export default ServiceIndex;
