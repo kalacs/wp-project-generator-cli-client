24cb792cecd6df24d073ec4aa7174a4ff2404c02
Zoltan Kakuk <kakuk.zoltan@e-vista.hu>
2019-10-31 22:59:59 +0100

    Refactor project generate command

---
+++ commands/project/kickstart.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Text} from 'ink';
+
+/// Kickstart project
+const ProjectKickstart = () => <Text>This is index of project command</Text>;
+
+
+export default ProjectKickstart;
